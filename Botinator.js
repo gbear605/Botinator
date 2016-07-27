6a63ac51cf97d5938ad86fc3ff643a960b9c4ff5
Garrison Taylor <garrisontaylor@gmail.com>
2016-07-27 06:05:04 -0400

    Add join timeout command

--- Botinator.js
+++ Botinator.js
@@ -4,6 +4,7 @@ function cleanUpText(text) {
 
 var lastVolume = API.getVolume(),
 	isMuted,
+	joinTimeout,
 	boop = new Audio('https://github.com/Gbear605/Botinator/raw/master/Boop.wav'),
 	autowoot = true,
 	autojoin = true,
@@ -155,6 +156,27 @@ function newCommand(data)
 	    		autojoin = true;
 	    		API.chatLog("auto join enabled");
 		}
+	}
+	
+	//sets autojoin timer 
+	// /jtime <time | minutes>
+	if (message[0] == '/jtime' && message.length() > 2)
+	{
+		time_minutes = message[1]
+		time = time_minutes * 60 * 1000;
+    		autojoin = true;
+    		API.chatLog("auto join with timeout of " + time_minutes + " started");
+		joinTimeout = setTimeout(function() {
+			autojoin = false;	
+		}, time);
+	}
+	
+	//clears autojoin timer 
+	// /jclear
+	if (message[0] == '/jclear')
+	{
+    		API.chatLog("auto join timeout cleared");
+		clearTimeout(joinTimeout);
 	}
 	
 	//toggle autowoot
